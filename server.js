db862f7bd11324664618447b7bb189df26dca462
w <2359590507@qq.com>
2017-02-13 23:07:03 +0800

    add server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use('/dist', express.static(__dirname + '/dist'));
+app.use('/templates', express.static(__dirname + '/templates'));
+app.use('/img', express.static(__dirname + '/img'));
+
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.listen('3000', function (){
+  console.log('Now is listening the port 3000');
+});
